12464911e31d0037283f32c8e2477a250963edee
shubhamAsolkar22 <shubham.asolkar21@gmail.com>
2021-10-16 11:40:47 +0530

    created state for application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React,{usestate} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList';
 
 function App() {
+  /* default state is empty array */
+  const [todo,setTodos] = usestate([]);
+
   return (
+    <>
     <TodoList />
+    <input type="text"/>
+    <button>add item</button>
+    <button>clear completed todo</button>
+    <div>0 left</div>
+    </>
   );
 }
 
