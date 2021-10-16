39a02db248cfe1b331191809cfb5626fc0d8c0c3
shubhamAsolkar22 <shubham.asolkar21@gmail.com>
2021-10-16 12:19:49 +0530

    futher development for event handling and uuid dependecy addition

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React,{usestate} from 'react';
+import React,{useState,useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList';
 
 function App() {
   /* default state is empty array */
-  const [todo,setTodos] = usestate([]);
+  const [todos,setTodos] = useState([{id:1, task: 'learn reactjs', completed : false},{id:2, task: 'learn investing', completed : false}]);
 
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value;
+    if(name === '')
+    return;
+    console.log(name);
+    setTodos(prevTodos => {
+      return [...prevTodos,{id:1,task:name,completed:false}];
+    });
+    todoNameRef.current.value = null;//to clear input field
+
+  }
   return (
     <>
-    <TodoList />
-    <input type="text"/>
-    <button>add item</button>
+    <TodoList todos={todos}/>
+    <input ref={todoNameRef} type="text"/>
+    <button onClick={handleAddTodo}>add item</button>
     <button>clear completed todo</button>
     <div>0 left</div>
     </>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import Todo from './Todo';
 
-export default function TodoList() {
+export default function TodoList({todos}) {
     return (
-        <div>
-            Prelimnary todo list
-        </div>
+        todos.map(todo => {return <Todo key={todo.id} todo = {todo}/>})
     )
 }
