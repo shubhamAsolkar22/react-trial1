975602850cc2c3578ebc4cc00c25ebc5b8897d25
shubhamAsolkar22 <shubham.asolkar21@gmail.com>
2021-10-16 13:04:09 +0530

    Added feature to strike out completed todos (not in tutorial)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
-import React,{useState,useRef} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList';
+import * as uuid from "uuid";
+
+const LOCAL_STORAGE_KEY = 'christ'
 
 function App() {
   /* default state is empty array */
-  const [todos,setTodos] = useState([{id:1, task: 'learn reactjs', completed : false},{id:2, task: 'learn investing', completed : false}]);
+  const [todos, setTodos] = useState([{ id: uuid.v4(), task: 'learn reactjs', completed: false }, { id: uuid.v4(), task: 'learn investing', completed: false }]);
 
   const todoNameRef = useRef();
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, []);
 
-  function handleAddTodo(e){
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddTodo(e) {
     const name = todoNameRef.current.value;
-    if(name === '')
-    return;
+    if (name === '')
+      return;
     console.log(name);
     setTodos(prevTodos => {
-      return [...prevTodos,{id:1,task:name,completed:false}];
+      return [...prevTodos, { id: uuid.v4(), task: name, completed: false }];
     });
     todoNameRef.current.value = null;//to clear input field
 
   }
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.completed = !todo.completed
+    setTodos(newTodos)
+  }
+  function handleStrikeOutTodo() {
+    const newTodos = [...todos]
+    newTodos.filter(todo => todo.completed).forEach(todo => todo.striked = true);
+    setTodos(newTodos)
+  }
+  
   return (
     <>
-    <TodoList todos={todos}/>
-    <input ref={todoNameRef} type="text"/>
-    <button onClick={handleAddTodo}>add item</button>
-    <button>clear completed todo</button>
-    <div>0 left</div>
+      <TodoList todos={todos} toggleTodo={toggleTodo}  />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>add item</button>
+      <button onClick={handleStrikeOutTodo}>strike out completed todo</button>
+      <div>{todos.filter(e => e.completed == false).length} left</div>
     </>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
-export default function Todo({todo}) {
+export default function Todo({ todo, toggleTodo }) {
+    function handleToggleTodo(e) {
+        toggleTodo(todo.id);
+    }
+    function getTask() {
+        if (todo.striked)
+            return <strike>{todo.task}</strike>
+        else return todo.task
+    }
     return (
         <div>
-        <label>
-          <input type="checkbox" checked={todo.complete} />
-          {todo.task}
-        </label>
-      </div>
+            <label>
+                <input type="checkbox" checked={todo.completed} onChange={handleToggleTodo} />
+                {getTask()}
+            </label>
+        </div>
     );
 }
